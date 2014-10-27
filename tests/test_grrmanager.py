bd63823be4681717520429b733442555725be232
kevin <kevin@stealsyour.pw>
2014-10-27 02:39:59 -0400

    better tests

--- tests/test_grrmanager.py
+++ tests/test_grrmanager.py
@@ -8,21 +8,17 @@ test_grrmanager
 Tests for `grrmanager` module.
 """
 
+import urllib2
 from flask import Flask
-from flask.ext.testing import TestCase
+from flask.ext.testing import LiveServerTestCase
+from app import app, db
 
-
-class Testgrrmanager(TestCase):
+class Testgrrmanager(LiveServerTestCase):
 
     def create_app(self):
-        app = Flask(__name__)
-        app.config['TESTING'] = True
+        app.config.from_object('config.TestConfiguration')
         return app
-
-    def test_something(self):
-        pass
-
-    def tearDown(self):
-        pass
-
-
+    
+    def test_server_is_up_and_running(self):
+        response = urllib2.urlopen(self.get_server_url())
+        self.assertEqual(response.code, 200)
